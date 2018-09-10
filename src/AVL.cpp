aed0b530645160d9f0c7d99d65e27d4c43896d2c
Ricardo Soro <sorojara@Umbreon>
2018-09-10 18:49:46 -0500

    mensaje

--- src/AVL.cpp
+++ src/AVL.cpp
@@ -19,10 +19,12 @@ void AVLtree::rotLL(nodo *punto){
   nodo *bufferZ = punto;
   nodo *bufferGeneral = bufferY -> RC;
   bufferY -> RC = bufferZ;
+  bufferZ -> Parent = bufferY;
   bufferZ -> LC = bufferGeneral;
+  bufferGeneral -> Parent = bufferZ;
   bufferY -> LC = bufferX;
+  bufferX -> Parent = bufferY;
   //falta asignar como hijo de padre
-  //falta asignar padres
 }
 
 void AVLtree::rotRR(nodo *punto){
@@ -32,38 +34,44 @@ void AVLtree::rotRR(nodo *punto){
   nodo *bufferZ = punto;
   nodo *bufferGeneral = bufferY -> LC;
   bufferY -> RC = bufferX;
+  bufferX -> Parent = bufferY;
   bufferZ -> RC = bufferGeneral;
+  bufferGeneral -> Parent = bufferZ;
   bufferY -> LC = bufferZ;
+  bufferZ -> Parent = bufferY;
   //falta asignar como hijo de padre
-  //falta asignar padres
 }
 
 void AVLtree::rotRL(nodo *punto){
   //nodo *padre = punto -> Parent;
   nodo *bufferY = punto -> RC;
-  nodo *bufferX = bufferY -> RC;
+  nodo *bufferX = bufferY -> LC;
   nodo *bufferZ = punto;
   nodo *bufferGeneral = bufferX -> RC;
   bufferZ -> RC = bufferX;
+  bufferX -> Parent = bufferZ;
   bufferX -> RC = bufferY;
+  bufferY -> Parent = bufferX;
   bufferY -> LC = bufferGeneral;
+  bufferGeneral -> Parent = bufferY;
   rotRR(bufferZ);
   //falta asignar como hijo de padre
-  //falta asignar padres
 }
 
 void AVLtree::rotLR(nodo *punto){
   //nodo *padre = punto -> Parent;
-  nodo *bufferY = punto -> RC;
+  nodo *bufferY = punto -> LC;
   nodo *bufferX = bufferY -> RC;
   nodo *bufferZ = punto;
   nodo *bufferGeneral = bufferX -> LC;
   bufferZ -> LC = bufferX;
+  bufferX -> Parent = bufferZ;
   bufferX -> LC = bufferY;
+  bufferY -> Parent = bufferX;
   bufferY -> RC = bufferGeneral;
+  bufferGeneral -> Parent = bufferY;
   rotLL(bufferZ);
   //falta asignar como hijo de padre
-  //falta asignar padres
 }
 
 
